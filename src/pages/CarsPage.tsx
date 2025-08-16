3b603acbc58f6bc1bce44b7f39a3c9c34422a5a0
boqitali <ismonaliyevboqitali7@gmail.com>
2025-08-16 15:55:07 +0500

    React query

--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,11 +1,9 @@
-import CarForm from "../components/CarForm";
 import CarList from "../components/CarList";
 
 export default function CarsPage() {
   return (
     <div className="p-4 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Cars CRUD</h1>
-      <CarForm />
       <CarList />
     </div>
   );
